7e7848c1d8d582dcd79f6da8529cc2b1500235b1
Patrick Auernig <patrick.auernig@mykolab.com>
2016-12-23 22:58:39 +0100

    update compile.rb

--- Rakefile
+++ Rakefile
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+require 'scss_lint/rake_task'
+
+SCSSLint::RakeTask.new
+
 task default: [:compile]
 
 task :compile do
-  ruby 'compile.rb'
+    ruby 'compile.rb'
 end
 
 task :copy do
-  ruby 'compile.rb -c'
+    ruby 'compile.rb -c'
+end
+
+task test: [:rubocop, :scsslint]
+
+task :rubocop do
+    sh 'rubocop'
 end
--- compile.rb
+++ compile.rb
@@ -8,13 +8,13 @@ require 'sass/plugin'
 options = {}
 
 parser =
-  OptionParser.new do |opts|
-    opts.banner = "Usage: #{__FILE__} [options]"
+    OptionParser.new do |opts|
+        opts.banner = "Usage: #{__FILE__} [options]"
 
-    opts.on('-c', '--copy', 'Copy generated output to clipboard') do |c|
-      options[:copy] = c
+        opts.on('-c', '--copy', 'Copy generated output to clipboard') do |c|
+            options[:copy] = c
+        end
     end
-  end
 
 parser.parse!
 
@@ -23,25 +23,18 @@ Sass::Plugin.options[:css_location] = './tmp/out'
 Sass::Plugin.options[:cache_location] = './tmp/sass_cache'
 Sass::Plugin.update_stylesheets
 
-css_base = <<~EOF
+stylish_css = <<~EOF
     @-moz-document domain(www.wanikani.com) {
-    %{css}
+#{File.read('./tmp/out/main.css')}
     }
 EOF
 
-files = Dir['./tmp/out/**/*.css'].sort
-puts "stitching together #{files.size} CSS files..."
-
-to_insert = files.map { |file| File.read(file) }.join("\n")
-
-stylish_css_doc = format(css_base, css: to_insert)
-
 open('./tmp/out.css', 'w') do |outfile|
-  outfile.truncate(0)
-  outfile.write(stylish_css_doc)
+    outfile.truncate(0)
+    outfile.write(stylish_css)
 end
 
 if options[:copy]
-  puts 'copying to clipboard...'
-  system('cat ./tmp/out.css | xsel --input --primary')
+    puts 'copying to clipboard...'
+    system('cat ./tmp/out.css | xsel --input --primary')
 end
